e72be9f8cb89dfbd298ef4b4538ca9cd3a312185
nibraas <nibraas@pop-os.localdomain>
2019-12-23 16:02:25 -0500

    first commit

---
+++ app/src/main/java/com/nibraas/oak/LoginRegister.kt
@@ -0,0 +1,32 @@
+package com.nibraas.oak
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoginRegister : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginRegister()
+    }
+
+    private lateinit var viewModel: LoginRegisterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_register_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LoginRegisterViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/nibraas/oak/LoginRegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.nibraas.oak
+
+import androidx.lifecycle.ViewModel
+
+class LoginRegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
